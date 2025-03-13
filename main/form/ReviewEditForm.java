73f13965a8372de16575a7b17eb62c6ec2e22a01
otsukieri11889 <otsukieri0610102@gmail.com>
2025-03-13 15:29:18 +0900

    Add files via upload

---
+++ main/form/ReviewEditForm.java
@@ -0,0 +1,22 @@
+package com.example.samuraitravel.form;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class ReviewEditForm {
+   @NotNull
+	private Integer id;
+   @NotNull(message = "評価を選択してください")
+   @Range(min = 1,max = 5,message ="評価は1～5のいずれかを選択してください")
+    private Integer rating;
+   @NotBlank(message ="コメントを入力してください")
+   @Length(max = 300,message ="コメントは300文字以内で入力してください")
+    private String review_text;		
+	}
+
+    
