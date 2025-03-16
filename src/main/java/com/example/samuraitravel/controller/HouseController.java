5048a34c3524a6299f359023147d436e3a8ba5d0
otsukieri11889 <otsukieri0610102@gmail.com>
2025-03-16 15:01:48 +0900

    Add files via upload

--- src/main/java/com/example/samuraitravel/controller/HouseController.java
+++ src/main/java/com/example/samuraitravel/controller/HouseController.java
@@ -109,6 +109,8 @@ public class HouseController {
         model.addAttribute("totalReviewCount", totalReviewCount);        
         
         return "houses/show";
-    }    
+    }  
+    
+
 }
     
